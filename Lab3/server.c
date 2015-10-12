ea22dc99441ba764b218ac962f6257f14c402a48
Alena <lipinskaya_alena@mail.ru>
2015-10-12 16:14:35 +0300

    BindAndListenServerSocket

--- Lab3/server.c
+++ Lab3/server.c
@@ -8,6 +8,7 @@
 int main(int argc, char* argv[])
 {
   int socket_id;
+  struct sockaddr_in server_addr
 
   if((socket_id = socket(AF_INET, SOCK_STREAM, 0)) == -1)
   {
@@ -17,7 +18,19 @@ int main(int argc, char* argv[])
   else
     printf("Socket was created\n");
 
-  
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(8888);
+
+  if(bind(socket_id,(struct sockaddr*)&server_addr,sizeof(server_addr)) < 0)
+  {
+    perror("ERROR. Bind faild\n");
+    return 1;
+  }
+  else
+    printf("Bind done\n");
+ 
+  listen(socket_id, 3);
 
   return 0;
 }
