a55ef93edf935f54a95f4da9ca503d97b9858895
victoriazakharo <annivanchikova@gmail.com>
2015-10-19 19:18:13 +0400

    ChangeStyle

--- Lab3/client.c
+++ Lab3/client.c
@@ -9,8 +9,7 @@ int main(int argc, char* argv[])
   struct sockaddr_in addr;
   char text[256], nameFile[256];
 
-  if((socket_id = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-  {
+  if ((socket_id = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
      perror("ERROR in creating socket\n");
      return 1;
   }
@@ -20,8 +19,7 @@ int main(int argc, char* argv[])
   addr.sin_family = AF_INET;
   addr.sin_port = htons(8888);
 
-  if(connect(socket_id, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-  {
+  if (connect(socket_id, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
      perror("ERROR. Connect failed\n");
      close(socket_id);
      return 1;
@@ -30,18 +28,16 @@ int main(int argc, char* argv[])
 
   printf("Enter name file:\n");
   scanf("%s", nameFile);
-  if(send(socket_id, nameFile, 256, 0) < 0)
-  {
+  if (send(socket_id, nameFile, 256, 0) < 0) {
       perror("Send nameFile failed\n");
       close(socket_id);
       return 1;
   }
   
-  while((read_size = recv(socket_id, text, 256, 0)) > 0)
+  while ((read_size = recv(socket_id, text, 256, 0)) > 0)
      printf("%s\n",text);       
 
-  if(read_size < 0)
-  {
+  if (read_size < 0) {
      perror("recv failed\n");
      close(socket_id);
      return 1;
--- Lab3/server.c
+++ Lab3/server.c
@@ -14,21 +14,18 @@ void *function(void* param)
   FILE *file;
   read_size = recv(client_id, name_file, 256, 0);
 
-  if(read_size == 0)
-  {
+  if (read_size == 0) {
      perror("Client disconnected\n");
      close(client_id);
      return;
   }
-  else if(read_size == -1)
-  {
+  else if (read_size == -1) {
      perror("recv failed\n");
      close(client_id);
      return;
   }
   
-  if(access(name_file, F_OK) < 0)
-  {
+  if (access(name_file, F_OK) < 0) {
      perror("File not found\n");
      close(client_id);
      return; 
@@ -36,21 +33,18 @@ void *function(void* param)
 
   file = fopen(name_file, "r");
 
-  if(file == NULL)
-  {
+  if (file == NULL) {
      perror("ERROR in openning file\n");
      close(client_id);
      return;
   }
 
-  while(1)
-  {
+  while (1) {
      read_size = fread(text_in_file, 1, 256, file);
-     if(read_size > 0)
+     if (read_size > 0)
         send(client_id, text_in_file, 256, 0);
 
-     if(feof(file))
-     {
+     if (feof(file)) {
 	printf("File was sent\n");
 	break;
      }
@@ -72,8 +66,7 @@ int main(int argc, char* argv[])
 	pid_t process_id  = 0;
 #endif
 
-  if((socket_id = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-  {
+  if ((socket_id = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
     perror("ERROR in creating socket\n");
     return 1;
   }
@@ -84,8 +77,7 @@ int main(int argc, char* argv[])
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(8888);
 
-  if(bind(socket_id,(struct sockaddr*)&server_addr,sizeof(server_addr)) < 0)
-  {
+  if (bind(socket_id,(struct sockaddr*)&server_addr,sizeof(server_addr)) < 0) {
     perror("ERROR. Bind faild\n");
     return 1;
   }
@@ -97,24 +89,20 @@ int main(int argc, char* argv[])
   printf("waiting for incoming connections\n");
 
   c = sizeof(struct sockaddr_in);
-  while((client_id = accept(socket_id, (struct sockaddr *)&client_addr, (socklen_t *)&c)))
-  {	
+  while ((client_id = accept(socket_id, (struct sockaddr *)&client_addr, (socklen_t *)&c))) {	
     printf("Connection accepted\n");
-    if(client_id < 0)
-    {
+    if (client_id < 0) {
        perror("ERROR in accepting\n");
        return 1;
     }
     
 #ifdef THREAD
-	for(i = 0; i < 5; i++)
-	{
+	for (i = 0; i < 5; i++) {
 	     if(threads[i] == NULL)
 	        break;
 
 	}
-	if(pthread_create(&threads[i], NULL, function, (void*) client_id) < 0)
-	{
+	if (pthread_create(&threads[i], NULL, function, (void*) client_id) < 0) {
 	     printf("Thread wasn't created\n");
 	     continue;
 	}
@@ -122,13 +110,11 @@ int main(int argc, char* argv[])
 	printf("Thread was created\n");
 #elif defined(PROCESS)
 	process_id = fork();
-	if(process_id < 0)
-	{
+	if (process_id < 0) {
 	   printf("ERROR in create process\n");
 	   return 1;
 	}
-	else if(process_id == 0)
-	{
+	else if (process_id == 0) {
 	   function((void*)client_id);
 	    	return 0;
 	}
