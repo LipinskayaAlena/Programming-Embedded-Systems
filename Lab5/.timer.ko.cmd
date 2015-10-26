8462b5886b70cf5cb8d66654758026964c2954b3
Alena <lipinskaya_alena@mail.ru>
2015-10-26 17:06:39 +0300

    deletefiles

--- Lab5/.timer.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/alena/GitHub/Programming-Embedded-Systems/Lab5/timer.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/alena/GitHub/Programming-Embedded-Systems/Lab5/timer.ko /home/alena/GitHub/Programming-Embedded-Systems/Lab5/timer.o /home/alena/GitHub/Programming-Embedded-Systems/Lab5/timer.mod.o
--- Lab5/.tmp_versions/timer.mod
+++
@@ -1,2 +0,0 @@
-/home/alena/GitHub/Programming-Embedded-Systems/Lab5/timer.ko
-/home/alena/GitHub/Programming-Embedded-Systems/Lab5/timer.o
